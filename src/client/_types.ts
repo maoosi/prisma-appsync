8eac3dcd173387a73b199621654ce9572690e577
Sylvain <hello@sylvainsimao.fr>
2021-04-26 16:31:11 +1000

    fix: `has no exported member 'Prisma'`

--- src/client/_types.ts
+++ src/client/_types.ts
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 import { AuthModes, Operations, AuthActions } from './_constants'
 
 export type CustomPrismaClientOptions = {
