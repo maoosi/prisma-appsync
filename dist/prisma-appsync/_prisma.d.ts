015e3b3afc0344d6095a223b134c7480f629df7f
Sylvain <hello@sylvainsimao.fr>
2021-06-19 19:10:36 +1000

    fix: delete dist folder from repo

--- dist/prisma-appsync/_prisma.d.ts
+++
@@ -1,5 +0,0 @@
-import { PrismaClient, Prisma } from '@prisma/client';
-import { CustomPrismaClientOptions } from './_types';
-export declare class CustomPrismaClient extends PrismaClient<Prisma.PrismaClientOptions, 'query'> {
-    constructor({ connectionUrl, debug }: CustomPrismaClientOptions);
-}
