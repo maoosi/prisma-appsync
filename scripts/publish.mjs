3bc5e1e980c1e022fa2dd62d08c9e45edc48719a
Sylvain <hello@sylvainsimao.fr>
2022-06-27 21:44:19 +1000

    chore: updating scripts

--- scripts/publish.mjs
+++ scripts/publish.mjs
@@ -1,17 +1,73 @@
 #!/usr/bin/env zx
-import './env.mjs'
+import Listr from 'listr'
+import prompts from 'prompts'
 
-// run tests (incl. build)
-import './test.mjs'
+await $`zx scripts/env.mjs`
+$.verbose = false
 
-// cleanse package.json file
-console.log(chalk.blue('\nPublish :: Cleanse package.json\n'))
-await $`node scripts/_pkg.cleanse`
+const { tag } = await prompts({
+    type: 'select',
+    name: 'tag',
+    message: 'Select publish tag',
+    choices: [
+        { title: 'preview', value: 'preview' },
+        { title: 'beta', value: 'beta' },
+        { title: 'rc', value: 'rc' },
+        { title: 'latest', value: 'latest' },
+    ],
+    initial: 0,
+})
 
-// publish NPM package
-console.log(chalk.blue('\nPublish :: Publish NPM package\n'))
-await $`npm publish --dry-run`
+if (!tag) process.exit()
 
-// restore package.json file
-console.log(chalk.blue('\nPublish :: Restore package.json\n'))
-await $`node scripts/_pkg.restore`
+const latestPublished = String(await $`npm show prisma-appsync@${tag} version`).trim()
+
+const minorPos = latestPublished.lastIndexOf('.')
+const possibleFutureVersion = `${latestPublished.slice(0, minorPos)}.${
+    parseInt(latestPublished.slice(minorPos + 1)) + 1
+}`
+
+const { publishVersion } = await prompts({
+    type: 'text',
+    name: 'publishVersion',
+    message: `Enter new version for @${tag}? (latest = "${latestPublished}")`,
+    initial: possibleFutureVersion,
+})
+
+if (!publishVersion || publishVersion === latestPublished) process.exit()
+
+const { versionOk } = await prompts({
+    type: 'confirm',
+    name: 'versionOk',
+    message: `Run "npm publish prisma-appsync --tag ${tag}" with pkg version "${publishVersion}"?`,
+    initial: false,
+})
+
+if (versionOk) {
+    await new Listr([
+        {
+            title: 'Running tests',
+            task: async () => await $`zx scripts/test.mjs`,
+        },
+        {
+            title: 'Cleansing package.json',
+            task: async () => await $`node scripts/_pkg.cleanse`,
+        },
+        {
+            title: `Setting publish version to ${publishVersion}`,
+            task: async () => {
+                let pkg = await fs.readJson('./package.json')
+                pkg.version = publishVersion
+                await fs.writeJson('./package.json', pkg)
+            },
+        },
+        {
+            title: 'Publishing on NPM',
+            task: async () => await $`npm publish prisma-appsync --tag ${tag}`,
+        },
+        {
+            title: 'Restoring package.json',
+            task: async () => await $`node scripts/_pkg.restore`,
+        },
+    ]).run()
+}
