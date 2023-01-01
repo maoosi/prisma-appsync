5082b8cffd86605c2aaf833362bb280ed22a47b3
Sylvain <hello@sylvainsimao.fr>
2023-01-01 22:29:15 +1000

    wip: unit tests for Generator (test Prisma to GraphQL conversion)
    https://github.com/maoosi/prisma-appsync/issues/84

--- tests/generator/prisma-to-graphql.spec.ts
+++ tests/generator/prisma-to-graphql.spec.ts
@@ -12,7 +12,7 @@ const tester = new EasyGraphQLTester(schema)
 
 describe('GENERATOR #gql', () => {
     describe('.basicOperations?', () => {
-        test('expect get<Model> query to exist, with the right params', async () => {
+        test('expect "get<Model>" query to be valid', async () => {
             const query = `
                 query ($id: Int!) {
                     getPost(where: { id: $id }) {
@@ -27,9 +27,138 @@ describe('GENERATOR #gql', () => {
             `
             tester.test(true, query, { id: 1 })
         })
+        test('expect "list<Model>" query to be valid', async () => {
+            const query = `
+                query {
+                    listUsers {
+                        uuid
+                        username
+                        email
+                        role
+                    }
+                }
+            `
+            tester.test(true, query)
+        })
+        test('expect "count<Model>" query to be valid', async () => {
+            const query = `
+                query {
+                    countUsers
+                }
+            `
+            tester.test(true, query)
+        })
     })
     describe('.advancedOperations?', () => {
-        test('expect custom resolver query to exist, with the right params', async () => {
+        test('expect "deeply nested read" query to be valid', async () => {
+            const query = `
+                query {
+                    listUsers {
+                        uuid
+                        username
+                        email
+                        role
+
+                        posts {
+                            id
+                            title
+                            authorUuid
+                            published
+                            views
+                            lastSavedAt
+                            
+                            comments {
+                                id
+                                message
+                                lastSavedAt
+                            }
+                        }
+                    }
+                }
+            `
+            tester.test(true, query)
+        })
+        test('expect "nested write" query to be valid', async () => {
+            const query = `
+                mutation {
+                    createUser(
+                        data: {
+                            username: "username"
+                            email: "email@gmail.com"
+
+                            posts: {
+                                create: [
+                                    {
+                                        id: 1
+                                        title: "Post 1" 
+                                    },
+                                    { 
+                                        id: 2
+                                        title: "Post 2"
+                                    }
+                                ]
+                            }
+                        }
+                    ) {
+                        uuid
+                        username
+                        email
+                        role
+
+                        posts {
+                            id
+                            title
+                            authorUuid
+                            published
+                            views
+                            lastSavedAt
+                        }
+                    }
+                }
+            `
+            tester.test(true, query)
+        })
+        test('expect "relation to one filter" query to be valid', async () => {
+            const query = `
+                query {
+                    listComments(
+                        where: {
+                            author: {
+                                username: {
+                                    equals: "username"
+                                }
+                            }
+                        }
+                    ) {
+                        message
+                    }
+                }
+            `
+            tester.test(true, query)
+        })
+        test('expect "relation to many filter" query to be valid', async () => {
+            const query = `
+                query {
+                    listUsers(
+                        where: {
+                            posts: {
+                                every: {
+                                    published: {
+                                        equals: true
+                                    }
+                                }
+                            }
+                        }
+                    ) {
+                        username
+                        email
+                        role
+                    }
+                }
+            `
+            tester.test(true, query)
+        })
+        test('expect "custom resolver" query to be valid', async () => {
             const query = `
                 mutation ($message: String!) {
                     notify(message: $message) {
